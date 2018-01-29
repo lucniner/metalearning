49373aec11fb96ad553c088b452cc6e4e7fb8bfa
David Molnar <david.bese.molnar@gmail.com>
2018-01-29 17:02:45 +0100

    rename extractor

--- src/main/java/at/hwl/machinelearning/ass3/metalearning/utils/SharedConstants.java
+++ src/main/java/at/hwl/machinelearning/ass3/metalearning/utils/SharedConstants.java
@@ -7,7 +7,7 @@ public class SharedConstants {
   public static final String NUMBER_OF_INSTANCES = "number_of_instances";
   public static final String PROPORTION_OF_MISSING_VALUES = "proportion_of_missing_values";
   public static final String ENTROPY = "entropy";
-  public static final String STANDARD_DEVIATION = "standard_deviation";
+  public static final String STANDARD_DEVIATION_MEAN = "standard_deviation_mean";
   public static final String CLASS_LABEL = "class";
   public static final String VARIANCE_MEAN = "variance_mean";
   public static final String VARIANCE_STD = "variance_std";
